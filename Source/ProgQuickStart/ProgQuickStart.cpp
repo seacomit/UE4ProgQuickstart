1c42eacfe42121dd56a5fdc023f6d2009bb9ddfa
versatus <86527888+versatus@users.noreply.github.com>
2023-05-25 15:32:39 -0400

    C++ UE5 Quick Start Tutorial

---
+++ Source/ProgQuickStart/ProgQuickStart.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProgQuickStart.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProgQuickStart, "ProgQuickStart" );
---
+++ Source/ProgQuickStart/ProgQuickStartGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProgQuickStartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGQUICKSTART_API AProgQuickStartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
